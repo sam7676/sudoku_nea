e8a7579cde93a2f181ab4f3bdd2f0111b97587b4
Sam <sams421@icloud.com>
2022-10-30 18:04:09 +0000

    Generator extras

--- solver.py
+++ solver.py
@@ -490,9 +490,9 @@ class algorithm_x:
                         X[k].add(i)
 
 class generate(algorithm_x):
-    def __init__(x,attempts):
+    def __init__(x,attempts,grid_inp='000000000000000000000000000000000000000000000000000000000000000000000000000000000'):
         start_time = perf_counter()
-        final_grid = x.generate_complete()
+        final_grid = x.generate_complete(grid_inp)
         hardest_generate = []
         for i in range(attempts[1]):
             ans = x.remove_complete(store_grid(final_grid),attempts[0])
@@ -500,10 +500,10 @@ class generate(algorithm_x):
         hardest_generate.sort(key=lambda x: x[1])
         print(f"Generator: {round(perf_counter()-start_time,6)}s")
         x.ans = export_answer(hardest_generate[-1][0]) 
-    def generate_complete(x):
+    def generate_complete(x,grid_inp):
 
         #Create grid
-        grid = create_grid('000000000000000000000000000000000000000000000000000000000000000000000000000000000',complex=False)
+        grid = create_grid(grid_inp,complex=False)
         cord_locs = [[0,0],[0,1],[0,2],[0,3],[0,4],[0,5],[0,6],[0,7],[0,8],[1,0],[1,1],[1,2],[1,3],[1,4],[1,5],[1,6],[1,7],[1,8],[2,0],[2,1],[2,2],[2,3],[2,4],[2,5],[2,6],[2,7],[2,8],[3,0],[3,1],[3,2],[3,3],[3,4],[3,5],[3,6],[3,7],[3,8],[4,0],[4,1],[4,2],[4,3],[4,4],[4,5],[4,6],[4,7],[4,8],[5,0],[5,1],[5,2],[5,3],[5,4],[5,5],[5,6],[5,7],[5,8],[6,0],[6,1],[6,2],[6,3],[6,4],[6,5],[6,6],[6,7],[6,8],[7,0],[7,1],[7,2],[7,3],[7,4],[7,5],[7,6],[7,7],[7,8],[8,0],[8,1],[8,2],[8,3],[8,4],[8,5],[8,6],[8,7],[8,8]]
         while True:
 
