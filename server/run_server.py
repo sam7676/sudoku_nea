889b16d8c6216fe1056808ee3f03b801f82bd6d9
Sam <sams421@icloud.com>
2024-05-26 19:16:18 +0100

    Added a backend for the server to run on - currently includes a login system and handles client connections. Will soon manage multiplayer and image recognition tasks

---
+++ server/run_server.py
@@ -0,0 +1,59 @@
+from flask import Flask
+from flask_socketio import SocketIO, send, emit
+
+
+import database
+from secret import flask_key
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = flask_key
+socketio = SocketIO(app)
+
+
+# Checks whether a token is valid using database.py
+@socketio.on('check_token')
+def check_token(json):
+    token = json["token"]
+
+    send({"result":database.check_access_token_validity(token)})
+
+# Attempts to login
+@socketio.on('attempt_login')
+def attempt_login(json):
+    username = json["username"]
+    password = json["password"]
+
+    try:
+        playerID, token = database.attempt_login(username, password)
+        send({"id":playerID, "token":token})
+    except:
+        send({"id":None, "token":None})
+
+# Attempts to login
+@socketio.on('attempt_register')
+def attempt_register(json):
+    username = json["username"]
+    password = json["password"]
+
+    try:
+        playerID, token = database.create_account(username, password)
+        send({"id":playerID, "token":token})
+    except:
+        send({"id":None, "token":None})
+
+
+
+
+
+@socketio.on('connect')
+def connect():
+    print("connected")
+
+@socketio.on('disconnect')
+def disconnect():
+    print("disconnected")
+
+
+if __name__ == '__main__':
+    socketio.run(app)
