d3dce5d0199978da0b1b90ee683d1b601aeb1a0e
Sam <sams421@icloud.com>
2024-06-01 23:34:51 +0100

    Re-added sudoku object detection, improving speeds by 10x and adding extra user input (to ensure lack of error)

--- client/object_detection.py
+++ client/object_detection.py
@@ -1,27 +1,61 @@
 from ultralytics import YOLO
-import cv2
-
-# Having problems with making the image grayscale and maintaining channels
-
-
-
-path = "C:/Users/sams4/Downloads/temp3.png"
-image = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
-detection_model = 'client/grid_model.pt'
-model = YOLO(detection_model)
-
-print(image.shape)
-x, y = image.shape
-image.reshape((x,y,3))
+from digit_model_architecture import DigitNetwork
+import torch
+import torchvision.transforms as transforms
 
 
-def process(image):
-    results = model(image)
+grid_model_path = 'client/grid_model.pt'
+digit_model_path = 'client/digit_model.pt'
 
-    print("Processed")
+grid_model = YOLO(grid_model_path)
+digit_model = DigitNetwork()
+digit_model.load_state_dict(torch.load(digit_model_path))
+
+def process_digit(image):
+
+    # Load transforms
+    image_transform = transforms.Compose([transforms.ToTensor(), transforms.Grayscale(num_output_channels=1),])
+    
+    # Transform image
+    image_tensor = image_transform(image)
+
+    # Unsqueeze tensor (to fit in model)
+    image_tensor = image_tensor.unsqueeze(0)
+
+    # Predict using softmax
+    result = digit_model(image_tensor)
+
+    # Get index of max value
+    max_val = result.flatten()[0]
+    max_ind = 0
+    for ind, value in enumerate(result.flatten()):
+        if value > max_val:
+            max_val = value
+            max_ind = ind
+    
+
+    return str(max_ind)
+
+
+def process_grid(image):
+
+    resized_image = image.resize((576, 576))
+    grid = ''
+
+    for i in range(81):
+        y_top = i // 9
+        x_left = i % 9
+        cell = resized_image.crop((x_left * 64, y_top * 64, x_left * 64 + 64, y_top * 64 + 64))
+
+        grid += process_digit(cell)
+
+    return grid
+
+
+
+def process_canvas(image, return_type = 'grid'):
+    results = grid_model(image)
     for result in results:
-
-        result.show()
 
         x1, y1, x2, y2 = result.boxes.xyxy[0].tolist()
 
@@ -32,16 +66,20 @@ def process(image):
         y1 = round_int(y1)
         y2 = round_int(y2)
 
+        if return_type == 'grid':
 
-        # Cropping the image
-        cropped_img = image[y1:y2, x1:x2]
+            cropped_grid = image.crop((x1, y1, x2, y2))
 
-        # Stretching the image
-        resized_image = cv2.resize(cropped_img, (576, 576)) 
+            return process_grid(cropped_grid)
+        
+        elif return_type == 'bounds':
 
-        cv2.imshow('',resized_image)
+            return (x1, y1, x2, y2)
+        
+        else:
+            raise Exception('Invalid return type')
 
-        cv2.waitKey(0)
+    return None
 
-process(image)
+
 
